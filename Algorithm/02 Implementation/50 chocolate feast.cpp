0ff42c1e24f6055a7f60df1974db9e8eb59edcf1
Idooo <ridhoaditya925@gmail.com>
2024-02-02 23:28:26 +0800

    add 50

---
+++ Algorithm/02 Implementation/50 chocolate feast.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+int solving(int n, int c, int m){
+
+  int res=0, wrappers=0, choco=0;
+
+  wrappers = n/c;
+  res += wrappers; 
+  while(wrappers >= m){
+    choco = wrappers / m;
+    wrappers = (wrappers % m) + choco;
+    res += choco;
+  }
+
+
+  return res;
+}
+
+int main(){
+
+  int t,n,c,m;
+
+  cin >> t;
+  while(t--){
+    cin >> n >> c >> m;
+    cout << solving(n,c,m) << endl;
+  }
+}
